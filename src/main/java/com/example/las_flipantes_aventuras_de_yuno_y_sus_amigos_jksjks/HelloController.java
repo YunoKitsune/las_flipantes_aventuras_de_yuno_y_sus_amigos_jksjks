be8ef5d96af91384e32000f1f1e66ce98138ebab
Yuno Kitsune <elar4cnid007@gmail.com>
2024-02-06 17:50:59 -0600

    Aqui empieza la aventura osiosi >w<

---
+++ src/main/java/com/example/las_flipantes_aventuras_de_yuno_y_sus_amigos_jksjks/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.las_flipantes_aventuras_de_yuno_y_sus_amigos_jksjks;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.las_flipantes_aventuras_de_yuno_y_sus_amigos_jksjks {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.las_flipantes_aventuras_de_yuno_y_sus_amigos_jksjks to javafx.fxml;
+    exports com.example.las_flipantes_aventuras_de_yuno_y_sus_amigos_jksjks;
+}
